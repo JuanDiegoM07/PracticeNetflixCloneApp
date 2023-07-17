a4158a8cb454f9819f6c4511e465ebf9b41bc1d8
JuanDiegoM07 <juandiegomanin001@gmail.com>
2023-07-16 19:21:46 -0700

    AddedTitleCollectionView

--- NetflixClone/Controllers/UpcomingViewController.swift
+++ NetflixClone/Controllers/UpcomingViewController.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
-
+        view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
     }
-    
 
 }
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+    }
+    
+}
+
+
